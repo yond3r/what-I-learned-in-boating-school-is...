a1a0166f7c8bef36fe30cee7f9596037eedf7aa0
Hope Barrow <barr.writing@gmail.com>
2022-08-25 00:28:00 -0500

    debugging + mini fixes

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -12,6 +12,9 @@ module.exports = () => {
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js',
+      database: './src/js/database.js',
+      editor: './src/js/editor.js',
+      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
@@ -31,6 +34,7 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
+        name: 'Just Another Text Editor',
         short_name: 'JATE',
         description: 'Just another text editor',
         background_color: '#225ca3',
